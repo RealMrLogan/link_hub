08bc88b6bbec73a1f69de59af1b7d340a1caf71f
Logan <lsaunders275@gmail.com>
2024-02-12 23:08:16 -0700

    add linkedin

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   links: [
     { label: 'Portfolio', href: 'https://github.com/RealMrLogan' },
-    { label: 'Blog', href: 'https://blog.mrlogan.dev/' }
+    { label: 'Blog', href: 'https://blog.mrlogan.dev/' },
+    { label: 'LinkedIn', href: 'https://www.linkedin.com/in/logan-saunders-441bb2177' }
   ]
 }
